35e3c8c51f7205eca3adbe59d380cc56729dac0a
fengmlo <fengmlo@qq.com>
2018-04-17 11:25:38 +0800

    DSL；

--- src/me/fengmlo/kotlin/dsl/DSL.kt
+++ src/me/fengmlo/kotlin/dsl/DSL.kt
@@ -1,7 +1,5 @@
 package me.fengmlo.kotlin.dsl
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.TagName
-
 interface Element {
     fun render(builder: StringBuilder, indent: String)
 }
@@ -46,13 +44,92 @@ abstract class Tag(val name: String) : Element {
         render(builder, "")
         return builder.toString()
     }
+}
 
-    abstract class TagWithText(name: String) : Tag(name) {
-        operator fun String.unaryPlus() {
-            children.add(TextElement(this))
-        }
+abstract class TagWithText(name: String) : Tag(name) {
+    operator fun String.unaryPlus() {
+        children.add(TextElement(this))
     }
+}
 
+class HTML : TagWithText("html") {
+    fun head(init: Head.() -> Unit) = initTag(Head(), init)
 
+    fun body(init: Body.() -> Unit) = initTag(Body(), init)
+}
 
+class Head : TagWithText("head") {
+    fun title(init: Title.() -> Unit) = initTag(Title(), init)
+}
+
+class Title : TagWithText("title")
+
+abstract class BodyTag(name: String) : TagWithText(name) {
+    fun b(init: B.() -> Unit) = initTag(B(), init)
+    fun p(init: P.() -> Unit) = initTag(P(), init)
+    fun h1(init: H1.() -> Unit) = initTag(H1(), init)
+    fun ul(init: UL.() -> Unit) = initTag(UL(), init)
+    fun a(href: String, init: A.() -> Unit) {
+        val a = initTag(A(), init)
+        a.href = href
+    }
+}
+
+class Body : BodyTag("body")
+class UL() : BodyTag("ul") {
+    fun li(init: LI.() -> Unit) = initTag(LI(), init)
+}
+class B : BodyTag("b")
+class LI() : BodyTag("li")
+class P : BodyTag("p")
+class H1 : BodyTag("h1")
+
+class A : BodyTag("a") {
+    var href: String
+        get() = attributes["herf"]!!
+        set(value) {
+            attributes["herf"] = value
+        }
+}
+
+fun html(init: HTML.() -> Unit): HTML {
+    val html = HTML()
+    html.init()
+    return html
+}
+
+fun main(args: Array<String>) {
+    val result =
+            html {
+                head {
+                    title { +"HTML encoding with Kotlin" }
+                }
+                body {
+                    h1 { +"HTML encoding with Kotlin" }
+                    p { +"this format can be used as an alternative markup to HTML" }
+
+                    // an element with attributes and text content
+                    a(href = "http://jetbrains.com/kotlin") { +"Kotlin" }
+
+                    // mixed content
+                    p {
+                        +"This is some"
+                        b { +"mixed" }
+                        +"text. For more see the"
+                        a(href = "http://jetbrains.com/kotlin") { +"Kotlin" }
+                        +"project"
+                    }
+                    p { +"some text" }
+
+                    // content generated from command-line arguments
+                    p {
+                        +"Command line arguments were:"
+                        ul {
+                            for (arg in args)
+                                li { +arg }
+                        }
+                    }
+                }
+            }
+    println(result)
 }
